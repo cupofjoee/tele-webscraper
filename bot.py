63927f3e075fe79d9c1701e571d9827e5170f6c7
Joe Teddy <jteddysanjaya@gmail.com>
2019-11-04 23:52:43 +0800

    add threading to regularly update attendance data

--- bot.py
+++ bot.py
@@ -1,22 +1,37 @@
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import scraper as sc
+import threading
+import time
+from datetime import datetime
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
+data = []
+
+def timer():
+    threading.Timer(90, timer).start()
+    global data
+    start = datetime.now()
+    data = sc.fetch_data()
+    end = datetime.now()
+    runtime  = end - start
+    print("Fetching data")
+    print("Runtime: " + str(runtime))
+
 def start(update, context):
     update.message.reply_text("Kobe Bryant")
 
 def fetch(update, context):
-    data = sc.fetch_data()
     for msg in data:
         update.message.reply_text(msg)
 
 def main():
     updater = Updater(token = '***REMOVED***', use_context = True)
+    timer()
 
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('start', start))
